a57734ff4f76ff1e4f3b48caf1e82e7b38d67ad3
Jam The Adventurer <94430800+JamTheAdventurer@users.noreply.github.com>
2022-05-19 18:15:28 +0800

    Update InfiniteJump.lua

--- .github/RobloxScripts/InfiniteJump.lua
+++ .github/RobloxScripts/InfiniteJump.lua
@@ -8,30 +8,9 @@ game.StarterGui:SetCore("SendNotification",  {
 	Duration = 3;
 	Callback = NotificationBindable;
 })
-_G.infinjump = true
- 
-local Player = game:GetService("Players").LocalPlayer
-local Mouse = Player:GetMouse()
-Mouse.KeyDown:connect(function(k)
-if _G.infinjump then
-if k:byte() == 32 then
-Humanoid = game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
-Humanoid:ChangeState("Jumping")
-wait(0.1)
-Humanoid:ChangeState("Seated")
-end
-end
-end)
- 
-local Player = game:GetService("Players").LocalPlayer
-local Mouse = Player:GetMouse()
-Mouse.KeyDown:connect(function(k)
-k = k:lower()
-if k == "M" then
-if _G.infinjump == true then
-_G.infinjump = false
-else
-_G.infinjump = true
-end
-end
+
+game:GetService("UserInputService").JumpRequest:connect(function()
+	if InfiniteJumpEnabled then
+		game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
+	end
 end)

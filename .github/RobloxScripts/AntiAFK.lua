f2dbd705221616846063d73667fa0a19f7b0687f
Jam The Adventurer <94430800+JamTheAdventurer@users.noreply.github.com>
2022-05-19 19:02:22 +0800

    Update AntiAFK.lua

--- .github/RobloxScripts/AntiAFK.lua
+++ .github/RobloxScripts/AntiAFK.lua
@@ -1,3 +1,14 @@
+local NotificationBindable = Instance.new("BindableFunction")
+NotificationBindable.OnInvoke = callback
+--
+game.StarterGui:SetCore("SendNotification",  {
+	Title = "Dynamic Client";
+	Text = "Anti-AFK Activated.";
+	Icon = "rbxassetid://2172412880";
+	Duration = 3;
+	Callback = NotificationBindable;
+})
+
 wait(3)
 local VirtualUser=game:service'VirtualUser'
 game:service('Players').LocalPlayer.Idled:connect(function()

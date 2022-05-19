1c883645d804e5c65baf688c2d0af7271ba5ea14
Jam The Adventurer <94430800+JamTheAdventurer@users.noreply.github.com>
2022-05-19 18:40:14 +0800

    Create GodMode.lua

---
+++ .github/RobloxScripts/GodMode.lua
@@ -0,0 +1,21 @@
+local NotificationBindable = Instance.new("BindableFunction")
+NotificationBindable.OnInvoke = callback
+--
+game.StarterGui:SetCore("SendNotification",  {
+	Title = "Dynamic Client";
+	Text = "GodMode Activated.";
+	Icon = "rbxassetid://295560883";
+	Duration = 3;
+	Callback = NotificationBindable;
+})
+game.Players.LocalPlayer.Character.Humanoid.Name = 1
+local l = game.Players.LocalPlayer.Character["1"]:Clone()
+l.Parent = game.Players.LocalPlayer.Character
+l.Name = "Humanoid"
+wait(0.1)
+game.Players.LocalPlayer.Character["1"]:Destroy()
+game.Workspace.CurrentCamera.CameraSubject = game.Players.LocalPlayer.Character
+game.Players.LocalPlayer.Character.Animate.Disabled = true
+wait(0.1)
+game.Players.LocalPlayer.Character.Animate.Disabled = false
+game.Players.LocalPlayer.Character.Humanoid.DisplayDistanceType = "None"

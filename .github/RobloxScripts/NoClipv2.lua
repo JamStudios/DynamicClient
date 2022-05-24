b3af4c6a864090c16ec29c7b480779c1641ad628
Jam The Adventurer <94430800+JamTheAdventurer@users.noreply.github.com>
2022-05-24 16:04:53 +0800

    Update NoClipv2.lua

--- .github/RobloxScripts/NoClipv2.lua
+++ .github/RobloxScripts/NoClipv2.lua
@@ -1,3 +1,14 @@
+local NotificationBindable = Instance.new("BindableFunction")
+NotificationBindable.OnInvoke = callback
+--
+game.StarterGui:SetCore("SendNotification",  {
+	Title = "Dynamic Client";
+	Text = "Noclip Activated, Press N to turn on/off.";
+	Icon = "rbxassetid://9097803303";
+	Duration = 3;
+	Callback = NotificationBindable;
+})
+
 noclip = false
 game:GetService('RunService').Stepped:connect(function()
 if noclip then

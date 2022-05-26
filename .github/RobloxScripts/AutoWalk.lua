313e5b4cb2a0a44be837f0e7639e31db33e386e5
Jam The Adventurer <94430800+JamTheAdventurer@users.noreply.github.com>
2022-05-26 14:05:38 +0800

    Update AutoWalk.lua

--- .github/RobloxScripts/AutoWalk.lua
+++ .github/RobloxScripts/AutoWalk.lua
@@ -1,1 +1,12 @@
+local NotificationBindable = Instance.new("BindableFunction")
+NotificationBindable.OnInvoke = callback
+--
+game.StarterGui:SetCore("SendNotification",  {
+	Title = "Dynamic Client";
+	Text = "Autowalk Activated";
+	Icon = "rbxassetid://8466853767";
+	Duration = 3;
+	Callback = NotificationBindable;
+})
+
 game.Players.LocalPlayer.Character.Humanoid:MoveTo(game.Players.LocalPlayer.Character.HumanoidRootPart.Position+workspace.Camera.CFrame.lookVector*100)

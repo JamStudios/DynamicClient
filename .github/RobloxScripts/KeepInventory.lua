c45a5fb0987244e3bf8eac0af7bc2b35724ae639
Jam The Adventurer <94430800+JamTheAdventurer@users.noreply.github.com>
2022-05-26 13:24:56 +0800

    Create KeepInventory.lua

---
+++ .github/RobloxScripts/KeepInventory.lua
@@ -0,0 +1,30 @@
+--[[
+Cyclically's Save Tools On Respawn
+After executing the script, please wait until you are able to move your character again.
+https://v3rmillion.net/member.php?action=profile&uid=785986
+
+this is so simple but some people wanted it
+]]
+
+-- Don't edit script unless you know what you're doing. If you wanna add this into a script, please give credits and message me on discord that you added it in a script at Cyclically#9215
+
+local LocalPlayer = game:GetService("Players").LocalPlayer
+local function saveTools()
+LocalPlayer.Character:WaitForChild("Humanoid").Died:Connect(function()
+LocalPlayer.Character:FindFirstChild("Humanoid"):UnequipTools()
+for _, tool in pairs(LocalPlayer.Backpack:GetChildren()) do
+if tool:IsA("Tool") then
+tool.Parent = LocalPlayer
+end
+end
+end)
+end
+LocalPlayer.CharacterAdded:Connect(function(char)
+for _, tool in pairs(LocalPlayer:GetChildren()) do
+if tool:IsA("Tool") then
+tool.Parent = LocalPlayer.Backpack
+end
+end
+saveTools()
+end)
+saveTools()

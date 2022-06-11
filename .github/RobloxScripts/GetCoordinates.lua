8f1d888a5c451805ca2eec8bf71b145a2277625a
Jam The Adventurer <94430800+JamTheAdventurer@users.noreply.github.com>
2022-06-11 14:58:51 +0800

    Update GetCoordinates.lua

--- .github/RobloxScripts/GetCoordinates.lua
+++ .github/RobloxScripts/GetCoordinates.lua
@@ -1,19 +1,10 @@
-local Players = game:GetService("Players")
-Players.PlayerAdded:Connect(function(player)
-	player.CharacterAdded:Connect(function(character)
-		local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
-		while humanoidRootPart do
-			local NotificationBindable = Instance.new("BindableFunction")
-			NotificationBindable.OnInvoke = callback
-			--
-			game.StarterGui:SetCore("SendNotification",  {
-				Title = "Dynamic Client";
-				Text = player.Name,"is at",tostring(humanoidRootPart.Position);
-				Icon = "rbxassetid://7825183423";
-				Duration = 3;
-				Callback = NotificationBindable;
-			})
-			wait(4)
-		end
-	end)
-end)
+
+local NotificationBindable = Instance.new("BindableFunction")
+NotificationBindable.OnInvoke = callback
+--
+game.StarterGui:SetCore("SendNotification",  {
+	Title = "Dynamic Client";
+	Text = game.Players.LocalPlayer.Character.HumanoidRootPart.Position;
+	Icon = "rbxassetid://7825183423";
+	Duration = 3;
+Callback = NotificationBindable;

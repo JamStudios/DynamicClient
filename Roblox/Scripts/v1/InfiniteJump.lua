14f5ab2e970d7e0df9f4903fd005c1a0f928100f
Jam The Adventurer (Official) <94430800+JamTheAdventurer@users.noreply.github.com>
2022-07-01 13:20:58 +0800

    Create InfiniteJump.lua

---
+++ Roblox/Scripts/v1/InfiniteJump.lua
@@ -0,0 +1,16 @@
+local NotificationBindable = Instance.new("BindableFunction")
+NotificationBindable.OnInvoke = callback
+--
+game.StarterGui:SetCore("SendNotification",  {
+	Title = "Dynamic Client";
+	Text = "Infinite Jump Activated.";
+	Icon = "rbxassetid://719941251";
+	Duration = 3;
+	Callback = NotificationBindable;
+})
+
+game:GetService("UserInputService").JumpRequest:connect(function()
+	if InfiniteJumpEnabled then
+		game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
+	end
+end)

